21078184cc23e265499919f23ca4488508558f8f
VectorTetra <viktochonov@gmail.com>
2024-12-11 18:40:31 +0200

    Виправлено стилі для адапитивів менше 400px в BlockShops.tsx

--- src/app/sharedComponents/BlockShops.tsx
+++ src/app/sharedComponents/BlockShops.tsx
@@ -1,21 +1,19 @@
 "use client";
-import React, { useState, useRef, useEffect } from "react";
-import styles from "./css/MenuShops.module.css";
-import useMainPageMenuShops from "@/store/mainPageMenuShops";
 import { ShopGetDTO, useShops } from "@/pages/api/ShopApi";
-import Link from "next/link";
 import useLocalStorageStore from "@/store/localStorage";
-import { useRouter } from "next/navigation";
-import { set } from "lodash";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import useMainPageMenuShops from "@/store/mainPageMenuShops";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./css/MenuShops.module.css";
+import ShopStatusInner from "./ShopStatusInner";
+import ShopStatusOuter from "./ShopStatusOuter";
 
 const BlockShops: React.FC = () => {
     const [searchTerm, setSearchTerm] = useState("");
-    const { selectedShop, setSelectedShop, setShopToViewOnShopPage } = useLocalStorageStore();
+    const { selectedShop, setSelectedShop } = useLocalStorageStore();
     const { isMainPageMenuShopsOpened, setIsMainPageMenuShopsOpened } = useMainPageMenuShops();
     const menuRef = useRef<HTMLDivElement | null>(null);
-    const router = useRouter();
 
     // Використання кешованих даних з API для отримання списку магазинів
     const { data: shops, isLoading } = useShops({
@@ -51,108 +49,9 @@ const BlockShops: React.FC = () => {
         }
     };
 
-    const checkShopStatus = (workHours: string) => {
-        const currentDate = new Date();
-        const currentDay = currentDate.toLocaleString("uk-UA", { weekday: "long" });
-        const currentTime = currentDate.getHours() * 60 + currentDate.getMinutes();
 
-        const workHoursArray = workHours.split("|").map((day) => {
-            const [dayweek, hours] = day.split(",");
-            const [open, close] = hours.split(" - ");
-            return { dayweek: dayweek.trim(), open: open.trim(), close: close.trim() };
-        });
 
-        const todayWorktime = workHoursArray.find((time) => time.dayweek.toLocaleLowerCase() === currentDay.toLocaleLowerCase());
-        if (todayWorktime) {
-            const [openHour, openMinute] = todayWorktime.open.split(":").map(Number);
-            const [closeHour, closeMinute] = todayWorktime.close.split(":").map(Number);
-            const openMinutes = openHour * 60 + openMinute;
-            const closeMinutes = closeHour * 60 + closeMinute;
 
-            if (currentTime >= openMinutes && currentTime < closeMinutes) {
-                return (
-                    <span style={{ marginLeft: "10px" }}>
-                        <span style={{ color: "green", fontWeight: "bold", fontSize: "0.9em" }}>Відчинено:</span>
-                        <span style={{ fontSize: "0.9em", marginLeft: "5px" }}> Зачиняється о {todayWorktime.close}</span>
-                    </span>
-                );
-            } else {
-                return (
-                    <span style={{ marginLeft: "10px" }}>
-                        <span style={{ color: "red", fontWeight: "bold", fontSize: "0.9em" }}>Зачинено:</span>
-                        <span style={{ fontSize: "0.9em" }}> Відкриється о {todayWorktime.open}</span>
-                    </span>
-                );
-            }
-        }
-        return "Час роботи не доступний";
-    };
-
-    const ShopStatus = ({ shop }: { shop: ShopGetDTO }) => {
-        const [isOpen, setIsOpen] = useState(false);
-
-        const toggleWorkHours = () => {
-            setIsOpen(!isOpen);
-        };
-
-        const parsedWorkHours = shop.workHours ? shop.workHours.split("|").map((day) => {
-            const [dayweek, hours] = day.split(",");
-            const [open, close] = hours.split(" - ");
-            return { dayweek: dayweek.trim(), open: open.trim(), close: close.trim() };
-        }) : [];
-
-        if (!isMainPageMenuShopsOpened) return null;
-        return (
-            <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%" }}>
-                    <div>{shop.workHours ? checkShopStatus(shop.workHours) : "Час роботи не доступний"}</div>
-                    <span
-                        onClick={toggleWorkHours}
-                        style={{
-                            margin: "10px 15px 10px 0",
-                            color: "#007bff",
-                            textDecoration: "underline",
-                            cursor: "pointer",
-                            fontSize: "14px",
-                        }}
-                    >
-                        Робочі години
-                    </span>
-                </div>
-                {isOpen && (
-                    <div style={{ width: "100%", padding: "10px", marginTop: "10px" }}>
-                        <div>
-                            <p style={{ fontWeight: "bold" }}>Інформація про магазин:</p>
-                            <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-                                <strong>Адреса:</strong>
-                                <span style={{ marginLeft: "50px" }}>
-                                    {shop.street},
-                                    <p style={{ marginBottom: 0 }}>{shop.city}</p>
-                                    <Link prefetch={true} className={styles.customlink} href="/shops">Як знайти магазин</Link>
-                                </span>
-                            </div>
-                        </div>
-                        <div style={{ display: "flex", margin: "20px 0 10px 0", flexDirection: "column" }}>
-                            <p style={{ fontWeight: "bold", marginBottom: "10px" }}>Робочі години:</p>
-                            <ul className={styles.worktimelist}>
-                                {parsedWorkHours.map((time, index) => (
-                                    <li key={index} className={styles.worktimeitem}>
-                                        <span className={styles.worktimeday}>{time.dayweek}:</span>
-                                        <span style={{ fontSize: "14px" }}>{time.open} - {time.close}</span>
-                                    </li>
-                                ))}
-                            </ul>
-                            <button className={styles.customlink2} onClick={() => {
-                                setShopToViewOnShopPage(shop);
-                                setIsMainPageMenuShopsOpened(false);
-                                router.push("/shop");
-                            }} >Показати магазин</button>
-                        </div>
-                    </div>
-                )}
-            </div>
-        );
-    };
 
     useEffect(() => {
         document.addEventListener("mousedown", handleClickOutside);
@@ -202,11 +101,12 @@ const BlockShops: React.FC = () => {
                                         <div className={styles.shopInfo}>
                                             <h2 className={styles.h2}>{shop.name}</h2>
                                         </div>
+                                        <ShopStatusInner shop={shop} />
                                         <button onClick={() => handleShopClick(shop)} className={styles.shopButton}>
                                             Обрати магазин
                                         </button>
                                     </div>
-                                    <ShopStatus shop={shop} />
+                                    <ShopStatusOuter shop={shop} />
                                 </div>
                             ))
                         )}
