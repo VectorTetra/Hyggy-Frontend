cb6ab8a7ca1acc73d5e6e04d1bfd896e8d290a05
VectorTetra <viktochonov@gmail.com>
2024-12-16 15:04:28 +0200

    Збереження перед мерджем

--- src/app/cart/success/page.tsx
+++ src/app/cart/success/page.tsx
@@ -1,24 +1,19 @@
 "use client";
-import { useState, useEffect } from "react";
-import Layout from "../../sharedComponents/Layout";
-import styles from "./page.module.css";
+import { AddressDTO } from "@/pages/api/AddressApi";
+import { OrderGetDTO, useCreateOrderByTransaction } from "@/pages/api/OrderApi";
+import { OrderDeliveryTypeGetDTO } from "@/pages/api/OrderDeliveryTypeApi";
+import { getDecodedToken, isUser, validateToken } from "@/pages/api/TokenApi";
 import useLocalStorageStore, { CartItem } from "@/store/localStorage";
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { AddressDTO, useAddresses, useCreateAddress } from "@/pages/api/AddressApi";
-import { OrderGetDTO, useCreateOrder, useOrders } from "@/pages/api/OrderApi";
-import { useOrderStatuses } from "@/pages/api/OrderStatusApi";
-import { useCreateOrFindGuestCustomer } from "@/pages/api/CustomerApi";
-import { getDecodedToken, isUser, validateToken } from "@/pages/api/TokenApi";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import { useCreateOrderItem } from "@/pages/api/OrderItemApi";
-import { set } from "lodash";
-import { OrderDeliveryTypeGetDTO } from "@/pages/api/OrderDeliveryTypeApi";
+import Layout from "../../sharedComponents/Layout";
+import styles from "./page.module.css";
 
 const SuccessPage = () => {
   const router = useRouter();
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
-  const [address, setAddress] = useState("");
   const [deliveryDate, setDeliveryDate] = useState("");
   const [deliveryCost, setDeliveryCost] = useState(0);
   const [createdOrderId, setCreatedOrderId] = useState(0);
@@ -27,23 +22,8 @@ const SuccessPage = () => {
 
   let selectedDeliveryType: OrderDeliveryTypeGetDTO | null = null;
   let selectedStore: any = null;
-
-
-  const { data: orderStatuses = [], isSuccess: isOrderStatusesSuccess } = useOrderStatuses({
-    SearchParameter: "Query",
-    PageNumber: 1,
-    PageSize: 1000
-  });
-  const { data: successfullOrders = [], } = useOrders<OrderGetDTO[]>({
-    SearchParameter: 'Query',
-    Id: createdOrderId,
-  }, createdOrderId !== 0 && orderCreationProcessComplete);
-
-  const successfullOrder = successfullOrders?.[0] || null; // Дістаємо перший елемент або null
-  const { mutateAsync: TryToCreateAddress } = useCreateAddress();
-  const { mutateAsync: TryToCreateOrder } = useCreateOrder();
-  const { mutateAsync: TryToCreateOrderItem } = useCreateOrderItem();
-  const { mutateAsync: TryToCreateGuestCustomer } = useCreateOrFindGuestCustomer();
+  const [successfullOrder, setSuccessfullOrder] = useState<OrderGetDTO | null>(null);
+  const { mutateAsync: TryToCreateOrderByTransaction } = useCreateOrderByTransaction();
 
   let AddressDTOtoPost = new AddressDTO();
 
@@ -51,136 +31,127 @@ const SuccessPage = () => {
 
   }, [router]);
   useEffect(() => {
-    const handleOrderCreate = async () => {
-      try {
-        if (paymentStatus !== 'success') {
-          router.push('/cart/payment');
-          return;
-        }
-        if (!deliveryInfo) {
-          router.push('/cart/delivery');
-          return;
-        }
-        if (!addressInfo) {
-          router.push('/cart/address');
-          return;
-        }
-
-        if (deliveryInfo && addressInfo) {
-          selectedDeliveryType = deliveryInfo.selectedDeliveryType;
-          selectedStore = deliveryInfo.selectedStore;
-          const orderDate = new Date();
-          const estimatedDeliveryDate = new Date(orderDate);
-          estimatedDeliveryDate.setDate(
-            orderDate.getDate() + (selectedDeliveryType?.maxDeliveryTimeInDays ?? 0)
-          );
-          const formattedDate = estimatedDeliveryDate.toLocaleDateString('uk-UA');
-
-          if (selectedDeliveryType?.id === 1) {
-            setAddress(
-              `${selectedStore.street}, ${selectedStore.houseNumber}, ${selectedStore.city}, ${selectedStore.postalCode}`
-            );
-            Object.assign(AddressDTOtoPost, {
-              Id: selectedStore.addressId,
-              City: selectedStore.city,
-              Street: selectedStore.street,
-              HouseNumber: selectedStore.houseNumber,
-              PostalCode: selectedStore.postalCode,
-              Latitude: selectedStore.latitude,
-              Longitude: selectedStore.longitude,
-            });
-          } else if (selectedDeliveryType?.id === 3) {
-            const [city, street, houseNumber] = selectedStore.address.split(',').map((s) => s.trim());
-            setAddress(`${selectedStore.address}, ${selectedStore.postalCode}`);
-            Object.assign(AddressDTOtoPost, {
-              City: city,
-              Street: street,
-              HouseNumber: houseNumber,
-              PostalCode: selectedStore.postalCode,
-              Latitude: selectedStore.latitude,
-              Longitude: selectedStore.longitude,
-            });
-          } else if (selectedDeliveryType?.id === 2) {
-            setAddress(
-              `${addressInfo.City}, ${addressInfo.Street}, ${addressInfo.HouseNumber}`
-            );
-            Object.assign(AddressDTOtoPost, {
-              City: addressInfo.City,
-              Street: addressInfo.Street,
-              HouseNumber: addressInfo.HouseNumber,
-            });
-          } else if (selectedDeliveryType?.id === 4) {
-            const [street, houseNumber] = selectedStore.address.split(',').map((s) => s.trim());
-            setAddress(`${selectedStore.address}, ${selectedStore.postalCode}`);
-            Object.assign(AddressDTOtoPost, {
-              City: selectedStore.city,
-              Street: street,
-              HouseNumber: houseNumber,
-              PostalCode: selectedStore.postalCode,
-              Latitude: selectedStore.latitude,
-              Longitude: selectedStore.longitude,
-            });
+    if (!successfullOrder) {
+      const handleOrderCreate = async () => {
+        try {
+          if (paymentStatus !== 'success') {
+            router.push('/cart/payment');
+            return;
+          }
+          if (!deliveryInfo) {
+            router.push('/cart/delivery');
+            return;
+          }
+          if (!addressInfo) {
+            router.push('/cart/address');
+            return;
           }
 
-          setDeliveryDate(formattedDate);
-          setDeliveryCost(selectedDeliveryType?.price ?? 0);
+          if (deliveryInfo && addressInfo) {
+            const orderDate = new Date();
+            const estimatedDeliveryDate = new Date(orderDate);
+            estimatedDeliveryDate.setDate(
+              orderDate.getDate() + (selectedDeliveryType?.maxDeliveryTimeInDays ?? 0)
+            );
+            const formattedDate = estimatedDeliveryDate.toLocaleDateString('uk-UA');
+
+            setDeliveryDate(formattedDate);
+            selectedDeliveryType = deliveryInfo.selectedDeliveryType;
+            selectedStore = deliveryInfo.selectedStore;
+            const validRegisteredCustomerId = validateToken().status === 200 && isUser() ? getDecodedToken()?.nameid : null;
+            switch (selectedDeliveryType?.id) {
+              case 1:
+                Object.assign(AddressDTOtoPost, {
+                  Id: selectedStore.addressId,
+                  City: selectedStore.city,
+                  Street: selectedStore.street,
+                  HouseNumber: selectedStore.houseNumber,
+                  PostalCode: selectedStore.postalCode,
+                  Latitude: selectedStore.latitude,
+                  Longitude: selectedStore.longitude,
+                });
+                break;
+              case 3:
+                const [city3, street3, houseNumber3] = selectedStore.address.split(',').map((s) => s.trim());
+                Object.assign(AddressDTOtoPost, {
+                  City: city3,
+                  Street: street3,
+                  HouseNumber: houseNumber3,
+                  PostalCode: selectedStore.postalCode,
+                  Latitude: selectedStore.latitude,
+                  Longitude: selectedStore.longitude,
+                });
+                break;
+              case 2:
+                Object.assign(AddressDTOtoPost, {
+                  City: addressInfo.City,
+                  Street: addressInfo.Street,
+                  HouseNumber: addressInfo.HouseNumber,
+                });
+                break;
+              case 4:
+                const [street4, houseNumber4] = selectedStore.address.split(',').map((s) => s.trim());
+                Object.assign(AddressDTOtoPost, {
+                  City: selectedStore.city,
+                  Street: street4,
+                  HouseNumber: houseNumber4,
+                  PostalCode: selectedStore.postalCode,
+                  Latitude: selectedStore.latitude,
+                  Longitude: selectedStore.longitude,
+                });
+                break;
+            }
+
+            const creationOrderDTO = {
+              RegisteredCustomerId: validRegisteredCustomerId ?? null,
+              GuestCustomer: !validRegisteredCustomerId ?
+                {
+                  Name: formData?.firstName!,
+                  Email: formData?.email!,
+                  Surname: formData?.lastName!,
+                  PhoneNumber: formData?.phone ? formData.phone.replace(/[^\d+]/g, '') : '',
+                } : null,
+              Address: {
+                City: addressInfo.City,
+                Street: addressInfo.Street,
+                HouseNumber: addressInfo.HouseNumber,
+              },
+              OrderData: {
+                Phone: formData?.phone ? formData.phone.replace(/[^\d+]/g, '') : '',
+                Comment: null,
+                ShopId: selectedShop?.id ?? 0,
+                DeliveryTypeId: selectedDeliveryType?.id ?? 0,
+              },
+              OrderItems: getCartFromLocalStorage().map((cartItem) => ({
+                WareId: cartItem.product.id,
+                Count: cartItem.quantity,
+                PriceHistoryId: cartItem.product.priceHistoryIds.findLast((x) => x)!,
+                OrderId: 0
+              }))
+            }
+
+            console.log('creationOrderDTO:', creationOrderDTO);
+            const createdOrder = await TryToCreateOrderByTransaction(creationOrderDTO);
+
+            setSuccessfullOrder(createdOrder);
+            clearCart();
+            setPaymentStatus(null);
+            setDeliveryInfo(null);
+            setAddressInfo(null);
+            setOrderCreationProcessComplete(true);
+            toast.success(`Замовлення успішно створено!`);
+          }
+
+
+        } catch (error) {
+          console.error('Error during order creation:', error);
+          toast.error(`Помилка при створенні замовлення! ${error.message}`);
         }
+      };
 
-        const validRegisteredCustomerId =
-          validateToken().status === 200 && isUser() ? getDecodedToken()?.nameid : null;
-
-        const CustomerId =
-          validRegisteredCustomerId ??
-          (
-            await TryToCreateGuestCustomer({
-              Name: formData?.firstName!,
-              Email: formData?.email!,
-              Surname: formData?.lastName!,
-              PhoneNumber: formData?.phone ? formData.phone.replace(/[^\d+]/g, '') : '',
-            })
-          ).id;
-
-        const createdAddress = await TryToCreateAddress(AddressDTOtoPost);
-        const createdOrder = await TryToCreateOrder({
-          StatusId: selectedDeliveryType?.id === 1 ? 9 : 1,
-          ShopId: selectedShop?.id ?? 0,
-          CustomerId: CustomerId,
-          Comment: null,
-          OrderDate: new Date(),
-          Phone: formData?.phone ? formData.phone.replace(/[^\d+]/g, '') : '',
-          DeliveryAddressId: createdAddress.id,
-          DeliveryTypeId: selectedDeliveryType?.id ?? 0,
-        });
-        setCreatedOrderId(createdOrder.id);
-
-        const cartItems = getCartFromLocalStorage();
-        setCartItems(cartItems);
-        for (const cartItem of cartItems) {
-          await TryToCreateOrderItem({
-            OrderId: createdOrder.id,
-            WareId: cartItem.product.id,
-            Count: cartItem.quantity,
-            PriceHistoryId: cartItem.product.priceHistoryIds.findLast((x) => x)!,
-          });
-        }
-
-        clearCart();
-        setPaymentStatus(null);
-        setDeliveryInfo(null);
-        setAddressInfo(null);
-        setOrderCreationProcessComplete(true);
-        toast.success(`Замовлення успішно створено!`);
-      } catch (error) {
-        console.error('Error during order creation:', error);
-        toast.error(`Помилка при створенні замовлення! ${error.message}`);
-      }
-    };
-
-    if (!orderCreationProcessComplete && isOrderStatusesSuccess) {
       handleOrderCreate();
-
     }
-  }, [router, isOrderStatusesSuccess, paymentStatus, deliveryInfo, addressInfo]);
+  }, [router, paymentStatus, deliveryInfo, addressInfo, successfullOrder]);
 
   if (!orderCreationProcessComplete || !successfullOrder) {
     return <Layout headerType="header1" footerType="footer1">
@@ -200,10 +171,14 @@ const SuccessPage = () => {
       </center>
       <center>
         <center>
-          <h6>Доставка за адресою:</h6>
-          {Object.entries(successfullOrder.deliveryAddress).map(([key, value], index) => (
-            <span key={index}>{value !== null && value.toString()} {index < Object.entries(successfullOrder.deliveryAddress).length}</span>
-          ))}
+          <h6>Доставка за адресою:&nbsp;
+            {Object.entries(successfullOrder.deliveryAddress)
+              .filter(([key]) => !["id", "latitude", "longitude", "shopId", "storageId", "orderIds"].includes(key))
+              .sort(([key1], [key2]) => key1.length < key2.length ? -1 : 1)
+              .map(([key, value], index, array) => (
+                <span key={index}>{value !== null && key !== "id" && `${value.toString()}${index + 1 < array.length ? "," : ''}`} {index < Object.entries(successfullOrder.deliveryAddress).length}</span>
+              ))}
+          </h6>
         </center>
         <h6> Очікувана дата доставки: до {deliveryDate}</h6>
       </center>
